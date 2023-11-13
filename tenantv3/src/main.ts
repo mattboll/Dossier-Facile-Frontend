e167ba0e72cf678060971ae8094173a5d46a01bf
Matthieu Bollot <m.bollot@code-troopers.com>
2023-11-13 13:38:03 +0100

    Add i18n

--- tenantv3/src/main.ts
+++ tenantv3/src/main.ts
@@ -5,11 +5,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import { createI18n } from 'vue-i18n'
-
-const i18n = createI18n({
-  // something vue-i18n options here ...
-})
+import i18n from './i18n';
 
 const app = createApp(App)
 
