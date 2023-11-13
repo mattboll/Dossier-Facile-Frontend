75891d80138da07de14660ed3b67d2e0bcb5fd0a
Matthieu Bollot <m.bollot@code-troopers.com>
2023-11-13 11:36:43 +0100

    wip

--- tenantv3/src/main.ts
+++ tenantv3/src/main.ts
@@ -5,10 +5,16 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { createI18n } from 'vue-i18n'
+
+const i18n = createI18n({
+  // something vue-i18n options here ...
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(i18n)
 
 app.mount('#app')
