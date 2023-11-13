e5d09ddefda3a8be9f5d8b4f18ea8cc79646d21d
Matthieu Bollot <m.bollot@code-troopers.com>
2023-11-13 15:17:06 +0100

    wip

---
+++ tenantv3/src/stores/tenant-store.ts
@@ -0,0 +1,18 @@
+
+// interface State {
+//   user: User = new User();
+//   selectedGuarantor = new Guarantor();
+//   status = { loggedIn: false };
+//   isFunnel = false;
+//   spouseAuthorize = false;
+//   coTenantAuthorize = false;
+//   coTenants: User[] = [];
+//   financialDocumentSelected = new FinancialDocument();
+//   editFinancialDocument = false;
+//   newMessage = 0;
+//   messageList: DfMessage[][] = [];
+//   skipLinks: SkipLink[] = [];
+//   guarantorFinancialDocumentSelected = new FinancialDocument();
+//   editGuarantorFinancialDocument = false;
+//   apartmentSharingLinks: ApartmentSharingLink[] = [];
+// }
