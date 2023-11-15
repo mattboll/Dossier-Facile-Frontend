1ca4afc4628a2eb0f18907dca1974dc6ba317231
Matthieu Bollot <m.bollot@code-troopers.com>
2023-11-15 11:24:05 +0100

    wip broken route

--- tenantv3/src/router/index.ts
+++ tenantv3/src/router/index.ts
@@ -1,23 +1,467 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { NavigationGuardNext, RouteLocationNormalized, createRouter, createWebHistory } from 'vue-router'
+import useTenantStore from '@/stores/tenant-store';
+import { CONTENT, SkipLink } from 'df-shared-next/src/models/SkipLink';
+import keycloak from '../plugin/keycloak';
+
+const MAIN_URL = `//${process.env.VUE_APP_MAIN_URL}`;
+const TENANT_URL = process.env.VUE_APP_FULL_TENANT_URL;
+
+let updateTokenInterval: NodeJS.Timer;
+
+const store = useTenantStore();
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    meta: {
+      title: "DossierFacile, le dossier de location numérique de l’État",
+      description:
+        "Créez un dossier de location en ligne complet et vérifié par l'Etat pour trouver votre appartement ou votre logement",
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  },
+  // {
+  //   path: "/signup",
+  //   name: "Signup",
+  //   meta: {
+  //     title: "Création de compte - DossierFacile",
+  //     description: "Créez votre compte en quelques clics sur DossierFacile",
+  //     hideForAuth: true,
+  //   },
+  //   component: () =>
+  //     import(
+  //       /* webpackChunkName: "signup" */
+  //       /* webpackPrefetch: true */
+  //       "../views/SignupPage.vue"
+  //     ),
+  // },
+  // {
+  //   path: "/login",
+  //   redirect: () => {
+  //     window.location.replace(`${TENANT_URL}/account`);
+  //     return "/account";
+  //   },
+  // },
+  // {
+  //   path: "/forgotten-password",
+  //   name: "ForgottenPassword",
+  //   meta: {
+  //     title: "Mot de passe oublié - DossierFacile",
+  //     description:
+  //       "Accédez à la procédure de mot de passe oublié pour votre compte DossierFacile",
+  //     hideForAuth: true,
+  //   },
+  //   component: () =>
+  //     import(
+  //       /* webpackChunkName: "forgottenPassword" */ "../views/ForgottenPasswordPage.vue"
+  //     ),
+  // },
+  // {
+  //   path: "/profile",
+  //   name: "Profile",
+  //   meta: {
+  //     title: "Édition du profil - DossierFacile",
+  //     requiresAuth: true,
+  //     hideFooter: true,
+  //     skipLinks: [CONTENT, FOOTER_NAVIGATION],
+  //   },
+  //   component: () =>
+  //     import(/* webpackChunkName: "profile" */ "../views/NameInformation.vue"),
+  // },
+  // {
+  //   path: "/nom-locataire",
+  //   name: "TenantName",
+  //   meta: {
+  //     title: "Édition du profil - DossierFacile",
+  //     requiresAuth: true,
+  //     hideFooter: true,
+  //     skipLinks: [CONTENT, FOOTER_NAVIGATION],
+  //   },
+  //   component: () =>
+  //     import(/* webpackChunkName: "profile" */ "../views/NameInformation.vue"),
+  // },
+  // {
+  //   path: "/type-locataire",
+  //   name: "TenantType",
+  //   meta: {
+  //     title: "Mes informations - DossierFacile",
+  //     requiresAuth: true,
+  //     hideFooter: true,
+  //     skipLinks: FUNNEL_SKIP_LINKS,
+  //   },
+  //   component: () =>
+  //     import(/* webpackChunkName: "profile" */ "../views/TypeInformation.vue"),
+  // },
+  // {
+  //   path: "/documents-locataire/:substep",
+  //   name: "TenantDocuments",
+  //   meta: {
+  //     title: "Mes documents - DossierFacile",
+  //     requiresAuth: true,
+  //     hideFooter: true,
+  //     skipLinks: FUNNEL_SKIP_LINKS,
+  //   },
+  //   component: () =>
+  //     import(/* webpackChunkName: "profile" */ "../views/TenantDocument.vue"),
+  // },
+  // {
+  //   path: "/documents-colocataire/:tenantId/:step/:substep/",
+  //   name: "CoTenantDocuments",
+  //   meta: {
+  //     title: "Édition du profil - DossierFacile",
+  //     requiresAuth: true,
+  //     hideFooter: true,
+  //     skipLinks: FUNNEL_SKIP_LINKS,
+  //   },
+  //   component: () =>
+  //     import(/* webpackChunkName: "profile" */ "../views/CoTenantDocument.vue"),
+  // },
+  // {
+  //   path: "/choix-garant",
+  //   name: "GuarantorChoice",
+  //   meta: {
+  //     title: "Mon garant - DossierFacile",
+  //     requiresAuth: true,
+  //     hideFooter: true,
+  //     skipLinks: FUNNEL_SKIP_LINKS,
+  //   },
+  //   component: () =>
+  //     import(
+  //       /* webpackChunkName: "profile" */ "../views/GuarantorChoicePage.vue"
+  //     ),
+  // },
+  // {
+  //   path: "/liste-garants",
+  //   name: "GuarantorList",
+  //   meta: {
+  //     title: "Mon garant - DossierFacile",
+  //     requiresAuth: true,
+  //     hideFooter: true,
+  //     skipLinks: FUNNEL_SKIP_LINKS,
+  //   },
+  //   component: () =>
+  //     import(
+  //       /* webpackChunkName: "profile" */ "../views/GuarantorListPage.vue"
+  //     ),
+  // },
+  // {
+  //   path: "/garants-locataire/:tenantId/:step",
+  //   name: "TenantGuarantors",
+  //   meta: {
+  //     title: "Édition du garant du locataire - DossierFacile",
+  //     requiresAuth: true,
+  //     hideFooter: true,
+  //     skipLinks: FUNNEL_SKIP_LINKS,
+  //   },
+  //   component: () =>
+  //     import(
+  //       /* webpackChunkName: "profile" */ "../views/TenantGuarantorsPage.vue"
+  //     ),
+  // },
+  // {
+  //   path: "/validation-dossier",
+  //   name: "ValidateFile",
+  //   meta: {
+  //     title: "Validation du dossier locataire - DossierFacile",
+  //     requiresAuth: true,
+  //     hideFooter: true,
+  //     skipLinks: FUNNEL_SKIP_LINKS,
+  //   },
+  //   component: () =>
+  //     import(/* webpackChunkName: "profile" */ "../views/ValidateFilePage.vue"),
+  // },
+  // {
+  //   path: "/validation-dossier/:step",
+  //   name: "ValidateFileStep",
+  //   meta: {
+  //     title: "Validation du dossier locataire - DossierFacile",
+  //     requiresAuth: true,
+  //     hideFooter: true,
+  //     skipLinks: FUNNEL_SKIP_LINKS,
+  //   },
+  //   component: () =>
+  //     import(/* webpackChunkName: "profile" */ "../views/ValidateFilePage.vue"),
+  // },
+  // {
+  //   path: "/info-garant/:substep/:guarantorId?",
+  //   name: "GuarantorDocuments",
+  //   meta: {
+  //     title: "Mon garant - DossierFacile",
+  //     requiresAuth: true,
+  //     hideFooter: true,
+  //     skipLinks: FUNNEL_SKIP_LINKS,
+  //   },
+  //   beforeEnter: async (to, from, next) => {
+  //     if (
+  //       (!store.state.selectedGuarantor.firstName ||
+  //         !store.state.selectedGuarantor.lastName) &&
+  //       to.params.substep !== "0"
+  //     ) {
+  //       next({ name: "GuarantorDocuments", params: { substep: "0" } });
+  //     }
+  //     next();
+  //   },
+  //   component: () =>
+  //     import(
+  //       /* webpackChunkName: "profile" */ "../views/GuarantorDocumentsPage.vue"
+  //     ),
+  // },
+  // {
+  //   path: "/info-garant-locataire/:tenantId/:guarantorId/:step/:substep?",
+  //   name: "TenantGuarantorDocuments",
+  //   meta: {
+  //     title: "Édition du garant du locataire - DossierFacile",
+  //     requiresAuth: true,
+  //     hideFooter: true,
+  //     skipLinks: FUNNEL_SKIP_LINKS,
+  //   },
+  //   component: () =>
+  //     import(
+  //       /* webpackChunkName: "profile" */ "../views/TenantGuarantorDocumentsPage.vue"
+  //     ),
+  // },
+  // {
+  //   path: "/public-file/:token",
+  //   name: "File",
+  //   meta: {
+  //     title: "Dossier - DossierFacile",
+  //   },
+  //   component: () =>
+  //     import(/* webpackChunkName: "file" */ "../views/PublicFile.vue"),
+  // },
+  // {
+  //   path: "/file/:token",
+  //   name: "PublicFile",
+  //   meta: {
+  //     title: "Dossier - DossierFacile",
+  //   },
+  //   component: () => import(/* webpackChunkName: "file" */ "../views/File.vue"),
+  // },
+  // {
+  //   path: "/account",
+  //   name: "Account",
+  //   meta: {
+  //     title: "Mon dossier - DossierFacile",
+  //     requiresAuth: true,
+  //   },
+  //   component: () =>
+  //     import(/* webpackChunkName: "account" */ "../views/Account.vue"),
+  // },
+  // {
+  //   path: "/applications",
+  //   name: "SharingPage",
+  //   meta: {
+  //     title: "Mes candidatures - DossierFacile",
+  //     requiresAuth: true,
+  //   },
+  //   component: () =>
+  //     import(/* webpackChunkName: "account" */ "../views/SharingPage.vue"),
+  // },
+  // {
+  //   path: "/messaging",
+  //   name: "Messages",
+  //   meta: {
+  //     title: "Messages - DossierFacile",
+  //     requiresAuth: true,
+  //   },
+  //   component: () =>
+  //     import(/* webpackChunkName: "messages" */ "../views/Messages.vue"),
+  // },
+  // {
+  //   path: "/confirmAccount/:token",
+  //   name: "Confirm",
+  //   meta: {
+  //     title: "Confirmation de compte - DossierFacile",
+  //     hideForAuth: true,
+  //   },
+  //   component: () =>
+  //     import(
+  //       /* webpackChunkName: "confirmAccount" */ "../views/ConfirmAccount.vue"
+  //     ),
+  // },
+  // {
+  //   path: "/ajout-couple/:token",
+  //   name: "Couple",
+  //   meta: {
+  //     title: "Confirmation de compte - DossierFacile",
+  //   },
+  //   component: () =>
+  //     import(/* webpackChunkName: "register" */ "../views/JoinCouple.vue"),
+  // },
+  // {
+  //   path: "/ajout-groupe/:token",
+  //   name: "Group",
+  //   meta: {
+  //     title: "Confirmation de compte - DossierFacile",
+  //   },
+  //   component: () =>
+  //     import(/* webpackChunkName: "register" */ "../views/JoinGroup.vue"),
+  // },
+  // {
+  //   path: "/reset-password/:token",
+  //   name: "Password",
+  //   meta: {
+  //     title: "Nouveau mot de passe - DossierFacile",
+  //     hideForAuth: false,
+  //   },
+  //   component: () =>
+  //     import(
+  //       /* webpackChunkName: "changePassword" */ "../views/ChangePasswordPage.vue"
+  //     ),
+  // },
+  // {
+  //   path: "/inscription-locataire/:token",
+  //   name: "Inscription",
+  //   meta: {
+  //     title: "Inscription locataire - DossierFacile",
+  //   },
+  //   beforeEnter: (to, from, next) => {
+  //     if (keycloak.authenticated) {
+  //       keycloak
+  //         .updateToken(70)
+  //         .then(() => {
+  //           store.dispatch("loadUser").then(() => {
+  //             next();
+  //           });
+  //         })
+  //         .catch((err: any) => {
+  //           console.error(err);
+  //         });
+  //     } else {
+  //       next();
+  //     }
+  //   },
+  //   component: () =>
+  //     import(
+  //       /* webpackChunkName: "inscriptionLocataire" */ "../views/OwnerShare.vue"
+  //     ),
+  // },
+  // {
+  //   path: "/locataire",
+  //   redirect: () => {
+  //     window.location.replace(`${MAIN_URL}`);
+  //     return "/info-proprietaire";
+  //   },
+  // },
+  // {
+  //   path: "/contact",
+  //   name: "Contact",
+  //   meta: {
+  //     title: "Contact - DossierFacile",
+  //     requiresAuth: false,
+  //   },
+  //   component: () =>
+  //     import(/* webpackChunkName: "contact" */ "../views/Contact.vue"),
+  // },
+  // {
+  //   path: "*",
+  //   name: "404",
+  //   meta: {
+  //     title: "404 - DossierFacile",
+  //   },
+  //   component: () =>
+  //     import(/* webpackChunkName: "404" */ "../views/NotFound404.vue"),
+  // },
+  ],
+  scrollBehavior() {
+    document.getElementById("app")?.scrollIntoView();
+  },
 })
 
-export default router
+function keepGoing(to: RouteLocationNormalized, next: NavigationGuardNext) {
+  if (
+    to.matched.some((record: { path: string }) => {
+      return record.path === "/account";
+    }) &&
+    store.user.status === "INCOMPLETE"
+  ) {
+    store.firstProfilePage();
+    return;
+  }
+  document.title = to.meta?.title as string;
+  if (to.meta?.description) {
+    const tag = document.querySelector('meta[name="description"]');
+    tag?.setAttribute("content", to.meta.description as string);
+
+    const prop = document.querySelector('meta[property="og:description"]');
+    prop?.setAttribute("content", to.meta.description as string);
+
+    const title = document.querySelector('meta[property="og:title"]');
+    title?.setAttribute("content", to.meta.title as string);
+  }
+  next();
+}
+
+function registerFunnel(to: RouteLocationNormalized) {
+  if (to.matched.some((record) => record.meta.hideFooter)) {
+    store.updateIsFunnel(true);
+  } else {
+    store.updateIsFunnel(false);
+  }
+}
+
+async function loadUserIfAuthenticated(next: NavigationGuardNext) {
+  if (!keycloak.authenticated) {
+    return;
+  }
+  await keycloak.loadUserProfile();
+  await store.loadUser().catch(() => {
+    next({ name: "404" });
+  });
+}
+
+function updateKeycloakTokenAndMessages() {
+  if (updateTokenInterval === undefined) {
+    updateTokenInterval = setInterval(() => {
+      keycloak.updateToken(60).catch((err: any) => {
+        console.error(err);
+      });
+    }, 45000);
+    store.updateMessages();
+  }
+}
+
+router.beforeEach(async (to: RouteLocationNormalized, from, next: NavigationGuardNext) => {
+  registerFunnel(to);
+
+  to.matched.some((record) => {
+    store.updateSkipLinks(record.meta.skipLinks as SkipLink[]);
+  });
+
+  // TODO
+  // const lang = Vue.$cookies.get("lang") === "en" ? "en" : "fr";
+  // store.dispatch("setLang", lang);
+
+  await loadUserIfAuthenticated(next);
+
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (!keycloak.authenticated) {
+      // The page is protected and the user is not authenticated. Force a login.
+      keycloak.login({
+        redirectUri: TENANT_URL + to.fullPath,
+      });
+    } else {
+      if (keycloak.profile?.emailVerified != true) {
+        // email should be validated before access to the protected page.
+        keycloak.logout({
+          redirectUri: "https:" + MAIN_URL + "/#emailNotValidated",
+        });
+      } else {
+        updateKeycloakTokenAndMessages();
+        keepGoing(to, next);
+        return;
+      }
+    }
+  } else if (to.matched.some((record) => record.meta.hideForAuth)) {
+    if (keycloak.authenticated) {
+      next({ name: "Profile" });
+    }
+  }
+  keepGoing(to, next);
+});
+
+export default router;
