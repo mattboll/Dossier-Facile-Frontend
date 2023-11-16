e959479c900e1e8e9e3accfeb302ff02e4f99fcc
Matthieu Bollot <m.bollot@code-troopers.com>
2023-11-16 13:22:21 +0100

    wip

--- tenantv3/src/components/TenantSkipLinks.vue
+++ tenantv3/src/components/TenantSkipLinks.vue
@@ -4,11 +4,11 @@
 
 <script setup lang="ts">
 import SkipLinks from "df-shared-next/src/components/SkipLinks.vue";
-import { SkipLink } from "df-shared-next/src/models/SkipLink";
-import useTenantStore from './stores/tenant-store';
+import useTenantStore from '../stores/tenant-store';
+import { computed } from "vue";
 
 const store = useTenantStore();
-const mySkipLink = computed(() => store.skipLinks);
+const mySkipLinks = computed(() => store.skipLinks);
 </script>
 
 <style scoped lang="scss"></style>
