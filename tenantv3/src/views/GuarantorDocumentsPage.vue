bd1087b78ab4ce02e42e6f97c64ce8e1afe365e9
Matthieu Bollot <m.bollot@code-troopers.com>
2023-11-21 17:13:30 +0100

    wip

--- tenantv3/src/views/GuarantorDocumentsPage.vue
+++ tenantv3/src/views/GuarantorDocumentsPage.vue
@@ -12,31 +12,33 @@ import { Guarantor } from "df-shared-next/src/models/Guarantor";
 import { computed, onBeforeMount, onBeforeUnmount, onMounted } from "vue";
 import { useRoute } from "vue-router";
 
-      const store = useTenantStore();
-      const guarantors = computed(() => {
-        return store.guarantors;
-      });
-      const route = useRoute();
+const store = useTenantStore();
+const guarantors = computed(() => {
+  return store.guarantors;
+});
+const route = useRoute();
 
-  onBeforeMount(async () => {
-    if (route.params.guarantorId) {
-      const g = guarantors.value.find((g: Guarantor) => {
-        return g.id?.toString() == route.params.guarantorId;
-      });
+onBeforeMount(async () => {
+  if (route.params.guarantorId) {
+    const g = guarantors.value.find((g: Guarantor) => {
+      return g.id?.toString() == route.params.guarantorId;
+    });
+    if (g) {
       await store.setSelectedGuarantor(g);
     }
-  })
-
-  onMounted(() => {
-    // window.Beacon("init", "e9f4da7d-11be-4b40-9514-ac7ce3e68f67");
-  })
-
-  onBeforeUnmount(() => {
-    // window.Beacon("destroy");
-  })
-  function getStep() {
-    return Number(route.params.substep) || 0;
   }
+})
+
+onMounted(() => {
+  // window.Beacon("init", "e9f4da7d-11be-4b40-9514-ac7ce3e68f67");
+})
+
+onBeforeUnmount(() => {
+  // window.Beacon("destroy");
+})
+function getStep() {
+  return Number(route.params.substep) || 0;
+}
 </script>
 
 <style lang="scss" scoped>
